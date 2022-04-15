aaf87179cbaff7831e16b104e7e75ffe715f829b
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-04-16 01:28:09 +0200

    get .env variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate dotenv;
+
+#[macro_use]
+extern crate dotenv_codegen;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    println!("{}", dotenv!("TWITCH_USERNAME"));
+    println!("{}", dotenv!("TWITCH_OAUTH"));
 }

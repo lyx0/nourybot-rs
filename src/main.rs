8a80d4d2a26a031d756005ed973a5ec163549ef0
lyx0 <66651385+lyx0@users.noreply.github.com>
2022-04-16 01:40:52 +0200

    join a channel and say something"

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,39 @@ extern crate dotenv;
 extern crate dotenv_codegen;
 
 use dotenv::dotenv;
+
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::TwitchIRCClient;
+use twitch_irc::{ClientConfig, SecureTCPTransport};
+
 use std::env;
 
-fn main() {
+#[tokio::main]
+pub async fn main() {
     dotenv().ok();
+
+    let login_name = dotenv!("TWITCH_USERNAME").to_owned();
+    let oauth_token = dotenv!("TWITCH_OAUTH").to_owned();
+    let config =
+        ClientConfig::new_simple(StaticLoginCredentials::new(login_name, Some(oauth_token)));
+
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            println!("Received message: {:?}", message);
+        }
+    });
+
+    client.join("pajlada".to_owned()).unwrap();
+
+    client
+        .say("pajlada".to_owned(), "test xd".to_owned())
+        .await
+        .unwrap();
+
+    join_handle.await.unwrap();
 
     println!("{}", dotenv!("TWITCH_USERNAME"));
     println!("{}", dotenv!("TWITCH_OAUTH"));
